a7eef1e600ddc0abdcba916fe1fe8190bf76a4b1
sunliguo <sunliguo2006@qq.com>
2023-02-15 18:19:31 +0800

    将重复的代码删除，导入tool

--- dahua.py
+++ dahua.py
@@ -4,11 +4,10 @@
 @contact: QQ376440229
 @Created on: 2022/2/17 22:31
 """
-from dahua_cv2 import dahua_cv2
 import csv
 from concurrent.futures import ThreadPoolExecutor
-from multiprocessing import Pool
-from threading import Thread
+
+from dahua_cv2 import dahua_cv2
 
 if __name__ == '__main__':
 
@@ -16,7 +15,5 @@ if __name__ == '__main__':
 
         csv_reader = csv.reader(fp)
         with ThreadPoolExecutor(10) as t:
-            for ip,password in csv_reader:
-
-                t.submit(dahua_cv2,ip,password)
-
+            for ip, password in csv_reader:
+                t.submit(dahua_cv2, ip, password)
--- hik_cv2.py
+++ hik_cv2.py
@@ -16,46 +16,16 @@ rtsp://[username]:[passwd]@[ip]:[port]/[codec]/[channel]/[subtype]/av_stream
 在 OpenCV 中，很简单就能读取 IP 摄像头。
 
 """
-import socket
 import os
 import time
+
+from tool import portisopen
 
 try:
     import cv2
 except Exception as e:
     os.system('pip install opencv-python')
     import cv2
-
-
-def portisopen(ip, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
-    state = sock.connect_ex((ip, port))
-    if 0 == state:
-        # print("port is open")
-        return True
-    else:
-        # print("port is closed")
-        return False
-
-
-def is_reachable(ip):
-    response = os.system('ping -c 1' + ip)
-    if response == 0:
-        print(ip + 'is reachable')
-        return 1
-    else:
-        print(ip + 'is not reachable')
-        return 0
-
-
-def is_ipv4(ip: str) -> bool:
-    """
-    检查ip是否合法
-    :param: ip ip地址
-    :return: True 合法 False 不合法
-    """
-    return True if [1] * 4 == [x.isdigit() and 0 <= int(x) <= 255 for x in ip.split(".")] else False
 
 
 def hik_cv2(ip="192.168.1.200", password='admin'):
