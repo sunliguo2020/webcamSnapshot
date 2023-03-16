394a076acaf0d011b5007f346d73532300b86e48
sunliguo <sunliguo2006@qq.com>
2023-03-16 22:10:44 +0800

    修复pycharm 提示shadow name 的错误

--- hik_cv2.py
+++ hik_cv2.py
@@ -35,20 +35,20 @@ logging.basicConfig(filename='hik_cv2.log',
                     format='%(asctime)s-%(filename)s[line:%(lineno)d]-%(message)s')
 
 
-def hik_cv2(ip="192.168.1.200", password='admin', dir_pre=''):
+def hik_cv2(cam_ip="192.168.1.200", cam_pwd='admin', dir_pre=''):
     """
     文件名保存的格式：ip_password_hik_timestr.jpg
 
-    :param ip: 摄像头ip地址
-    :param password:摄像头密码
+    :param cam_ip: 摄像头ip地址
+    :param cam_pwd:摄像头密码
     :param dir_pre: 保存目录前缀
     :return:
             -1:网络不通或者端口554没有打开
 
     """
-    if not portisopen(ip, 554):
-        print(f"{ip}:554 端口没有打开")
-        logging.debug(f'{ip} 554端口没开放或者网络不通！')
+    if not portisopen(cam_ip, 554):
+        print(f"{cam_ip}:554 端口没有打开")
+        logging.debug(f'{cam_ip} 554端口没开放或者网络不通！')
         return -1
 
     str_time = time.strftime("%Y%m%d%H%M%S", time.localtime())
@@ -58,29 +58,29 @@ def hik_cv2(ip="192.168.1.200", password='admin', dir_pre=''):
     if not os.path.isdir(pic_dir):
         os.makedirs(os.path.join('./', pic_dir))
 
-    pic_file_name = f"{ip}_{password}_hik_{str_time}.jpg"
+    pic_file_name = f"{cam_ip}_{cam_pwd}_hik_{str_time}.jpg"
     # 保存文件的路径名
     pic_full_path = os.path.join(pic_dir, pic_file_name)
 
     logging.debug(f'要保存的文件路名为：{pic_full_path}')
 
     try:
-        cam = cv2.VideoCapture("rtsp://admin:{}@{}:554/h264/ch34/main/av_stream".format(password, ip))
+        cam = cv2.VideoCapture("rtsp://admin:{}@{}:554/h264/ch34/main/av_stream".format(cam_pwd, cam_ip))
         ret, frame = cam.read()
         retval = cv2.imwrite(pic_full_path, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
         # if not retval:
         #     logging.debug(f'{ip}保存图像失败')
     except Exception as e:
-        logging.debug(f'{ip} cv2 失败{e}')
+        logging.debug(f'{cam_ip} cv2 失败{e}')
     finally:
         cam.release()
         cv2.destroyAllWindows()
 
         if not os.path.isfile(pic_full_path):
-            logging.info(f'{ip}保存截图失败')
+            logging.info(f'{cam_ip}保存截图失败')
             return -2
         else:
-            logging.info(f"{ip}保存截图成功")
+            logging.info(f"{cam_ip}保存截图成功")
             return 1
 
 
@@ -97,12 +97,14 @@ def gen_ip_password_from_csv(file_path, line_count=0):
             next(f)  # 跳过一行
 
         csv_read = csv.reader(f)
-        for ip, password in csv_read:
-            yield ip, password
+        for cam_ip, cam_pwd in csv_read:
+            yield cam_ip, cam_pwd
 
 
 if __name__ == "__main__":
+    # 包含ip和密码的csv文件
     csv_file = r'./txt/ruizhi.csv'
+    # 截图失败的IP地址
     failed_ip = []
 
     for count, (ip, password) in enumerate(gen_ip_password_from_csv(csv_file, 0), start=1):
