eef3d8474398a1e54f331bce6cc4866007f940ea
sunliguo <sunliguo2006@qq.com>
2023-05-19 11:17:05 +0800

    优化变量命名

--- cv2Snapshot.py
+++ cv2Snapshot.py
@@ -21,7 +21,7 @@ except ImportError as e:
     import cv2
 
 logging.basicConfig(filename='cv2.log',
-                    level=logging.DEBUG,
+                    level=logging.ERROR,
                     filemode='w',
                     format='%(asctime)s-%(filename)s[line:%(lineno)d]-%(message)s')
 
@@ -39,9 +39,9 @@ def cv2_video_capture(cam_ip, cam_pwd, cam_client=None, dir_pre=None):
         os.mkdir(dir_pre)
 
     # 判断rtsp协议的554端口有没有打开。
-    port_isopen_result = portisopen(cam_ip, 554)
-    logging.debug(f"判断{cam_ip}端口是否打开{type(port_isopen_result)},{port_isopen_result}")
-    if not port_isopen_result:
+    port_open_result = portisopen(cam_ip, 554)
+    logging.debug(f"判断{cam_ip}端口是否打开{type(port_open_result)},{port_open_result}")
+    if not port_open_result:
         logging.debug(f"{cam_ip} 554 端口没有打开或者网络不通")
         return -1
 
@@ -58,6 +58,7 @@ def cv2_video_capture(cam_ip, cam_pwd, cam_client=None, dir_pre=None):
     logging.debug(f"保存截图的目录:{pic_dir}")
 
     os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "timeout;5000"
+
     # 判断是海康还是大华的摄像头
     if cam_client == 'dahua':
         cam = cv2.VideoCapture("rtsp://admin:{}@{}:554/cam/realmonitor?channel=1&subtype=0".format(cam_pwd, cam_ip),
@@ -68,6 +69,7 @@ def cv2_video_capture(cam_ip, cam_pwd, cam_client=None, dir_pre=None):
     else:
         logging.error("设备类型参数不正确")
         return -1
+
     # logging.debug(cam)
     if cam.isOpened():  # 判断视频对象是否成功读取成功
         # 按帧读取视频，返回值ret是布尔型，正确读取则返回True，读取失败或读取视频结尾则会返回False。
@@ -76,7 +78,7 @@ def cv2_video_capture(cam_ip, cam_pwd, cam_client=None, dir_pre=None):
         try:
             # 添加水印信息
             text = snapshot_file_name.replace('.jpg', '')
-            fontFace = cv2.FONT_HERSHEY_SIMPLEX  # 字体
+            font_face = cv2.FONT_HERSHEY_SIMPLEX  # 字体
             line_type = cv2.LINE_AA
             '''
             如果对大小约为1000 x 1000的图像使用fontScale = 1，则此代码应正确缩放字体
@@ -87,10 +89,11 @@ def cv2_video_capture(cam_ip, cam_pwd, cam_client=None, dir_pre=None):
 
             # 计算文本的宽高 baseline
             # retval 返回值，元组，字体的宽高 (width, height)
-            retval, base_line = cv2.getTextSize(text, fontFace=fontFace, fontScale=font_scale, thickness=thickness)
+            retval, base_line = cv2.getTextSize(text, fontFace=font_face, fontScale=font_scale, thickness=thickness)
             print(f'retval:{retval},baseLine:{base_line}')
             print(f'font_face.shape:{frame.shape}')
             img_width = frame.shape[1]
+            img_hight= frame.shape[0]
             text_width = retval[0]
             # 如果文字的宽带大于图片的宽度，则缩小比例因子
             if text_width > img_width:
@@ -102,7 +105,7 @@ def cv2_video_capture(cam_ip, cam_pwd, cam_client=None, dir_pre=None):
             # 各参数依次是：图片，添加的文字，左上角坐标，字体，字体大小，颜色，字体粗细
             # 字体大小，数值越大，字体越大
             # 字体粗细，越大越粗，数值表示描绘的线条占有的直径像素个数
-            img2 = cv2.putText(frame, text, (0, 200), fontFace, font_scale, (100, 255, 0),
+            img2 = cv2.putText(frame, text, (0, img_hight*0.1), font_face, font_scale, (100, 255, 0),
                                thickness, line_type)
 
             retval = cv2.imwrite(snapshot_full_path, img2, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
@@ -137,7 +140,7 @@ def save_failed_ip(csv_file_name, failed_ip):
     """
     # 保存失败的ip记录到文件中
     failed_file = os.path.basename(csv_file_name).replace('.csv', '') + '_failed_' + time.strftime("%Y%m%d%H%M%S",
-                                                                                              time.localtime()) + '.csv'
+                                                                                                   time.localtime()) + '.csv'
     with open(failed_file, 'w', newline='') as fp:
         csv_writer = csv.writer(fp)
         csv_writer.writerows(failed_ip)
@@ -145,7 +148,7 @@ def save_failed_ip(csv_file_name, failed_ip):
 
 if __name__ == '__main__':
     # 包含ip和密码的csv文件
-    csv_file = r'./fenghua_failed_20230515161253.csv'
+    csv_file = r'./txt/ruizhi.csv'
     # hik or dahua
     client = 'hik'
 
@@ -170,23 +173,22 @@ if __name__ == '__main__':
             count += 1
             print(count, ":", ip)
             try:
+                # 截图并保存
                 result = cv2_video_capture(ip, password, client, dir_pre=os.path.basename(csv_file).split('.')[0])
-
             except Exception as e:
-                print(e)
-            # 统计下载失败的IP地址
-            if result < 0:
-                print(f"{ip}下载失败{item[2]}次")
-                item[2] += 1
-            elif result == 1:
-                print('截图成功，准备删除', item)
-                if ip not in success_ip:
-                    success_ip.append(ip)
-                # 删除这条ip
-                ip_passwd.remove(item)
+                print(f"截图过程中出错:{e}")
+            finally:
+                # 统计下载失败的IP地址
+                if result < 0:
+                    print(f"{ip}下载失败{item[2]}次")
+                    item[2] += 1
+                elif result == 1:
+                    print('截图成功，准备删除', item)
+                    if ip not in success_ip:
+                        success_ip.append(ip)
+                    # 删除这条ip
+                    ip_passwd.remove(item)
 
-    print(f'总共成功{len(success_ip)}个ip截图')
-
-    print(f'最后还有{len(ip_passwd)}个ip截图失败')
+    print(f'总共成功{len(success_ip)}个ip截图,{len(ip_passwd)}个ip截图失败')
 
     save_failed_ip(csv_file, ip_passwd)
