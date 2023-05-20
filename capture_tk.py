9fa2d1b6f697fa7df75625c753407d3780f83fbb
sunliguo <sunliguo2006@qq.com>
2023-05-20 12:55:43 +0800

    实现了基本功能

--- capture_tk.py
+++ capture_tk.py
@@ -5,7 +5,9 @@
 @Created on: 2023-05-19 18:13
 """
 import tkinter as tk
-from tkinter import filedialog,ttk
+from tkinter import filedialog, ttk
+
+from cv2Snapshot import cams_capture
 
 
 def select_file():
@@ -26,28 +28,50 @@ def select_folder():
     select_path.set(selected_folder)
 
 
+def start_cap():
+    # print(type(csv_entry.get()))
+    csv_file = csv_entry.get()
+    client_type = numberChosen.get()
+    print(client_type)
+    if client_type == '海康':
+        cams_capture(csv_file, client='hik')
+    elif client_type == '大华':
+        cams_capture(csv_file,client='dahua')
+
+
 root = tk.Tk()
 root.title('采集摄像头截图')
 
-root.geometry('500x300+300+200')  # 定义窗口显示大小和显示位置
-
+root.geometry('800x700+300+200')  # 定义窗口显示大小和显示位置
 
 # 初始化Entry控件的text variable属性值
 select_path = tk.StringVar()
 
 # 布局空间
-tk.Label(root, text='文件路径:').grid(column=0, row=0, rowspan=3)
-tk.Entry(root, textvariable=select_path).grid(column=1, row=0, rowspan=3)
+csv_file_label = tk.Label(root, text='文件路径:')
+csv_file_label.grid(column=0, row=0, rowspan=3)
+
+csv_entry = tk.Entry(root, textvariable=select_path)
+csv_entry.grid(column=1, row=0, rowspan=3)
+
 tk.Button(root, text="选择单个文件", command=select_file).grid(row=0, column=2)
 # tk.Button(root, text="选择多个文件", command=select_files).grid(row=1, column=2)
 # tk.Button(root, text="选择文件夹", command=select_folder).grid(row=2, column=2)
 
 # 创建一个下拉列表
-tk.Label(root, text='摄像头类型:').grid(column=0, row=2)
+tk.Label(root, text='摄像头类型:').grid(column=0, row=5, rowspan=3)
 number = tk.StringVar()
-numberChosen = ttk.Combobox(root, width=12, textvariable=number)
-numberChosen['values'] = ('海康','大华')     # 设置下拉列表的值
-numberChosen.grid(column=1, row=2)      # 设置其在界面中出现的位置  column代表列   row 代表行
-numberChosen.current(0)    # 设置下拉列表默认显示的值，0为 numberChosen['values'] 的下标
+numberChosen = ttk.Combobox(root,state='readonly', width=12, textvariable=number)
+numberChosen['values'] = ('海康', '大华')  # 设置下拉列表的值
+numberChosen.grid(column=1, row=5)  # 设置其在界面中出现的位置  column代表列   row 代表行
+numberChosen.current(0)  # 设置下拉列表默认显示的值，0为 numberChosen['values'] 的下标
+
+# 采集按钮
+capture_button = tk.Button(root, text='开始采集', bg='lightblue', width=10, command=start_cap)
+capture_button.grid(row=8, column=4)
+
+# 日志框
+log_data_text = tk.Text(root)
+log_data_text.grid(row=10, column=0, columnspan=10)
 
 root.mainloop()
