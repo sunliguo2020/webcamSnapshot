98246a90818adf9ae3e48da3ac1190ef0371b054
sunliguo <sunliguo2006@qq.com>
2023-06-21 23:55:08 +0800

    1、添加日志 2、try

--- tool.py
+++ tool.py
@@ -6,8 +6,12 @@
 将通用的函数放在一起。
 """
 import csv
+import logging
 import os
 import socket
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 def portisopen(ip, port):
@@ -48,14 +52,22 @@ def gen_ip_password_from_csv(file_path, line_count=0):
     :param line_count: 跳过前几行
     :return: ip,password
     """
+    # 判断是否是csv文件
     with open(file_path, encoding='utf-8') as f:
         # 跳过前几行
         for i in range(line_count):
             next(f)  # 跳过一行
 
-        csv_read = csv.reader(f)
-        for row in csv_read:
-            # 读取每行的前两列
-            cam_ip = row[0].strip()
-            cam_pwd = row[1].strip()
-            yield cam_ip, cam_pwd
+        try:
+            csv_read = csv.reader(f)
+            for row in csv_read:
+                # 读取每行的前两列
+                cam_ip = row[0].strip()
+                cam_pwd = row[1].strip()
+                yield cam_ip, cam_pwd
+        except UnicodeDecodeError as e:
+            logger.error(e)
+            return -1
+        except Exception as e:
+            logger.debug(e)
+            return -1
