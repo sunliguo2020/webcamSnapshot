e28eed5bf2643875cdcd46b0879c6956711a43a5
sunliguo <sunliguo2006@qq.com>
2023-06-21 23:13:22 +0800

    自动生成exe的脚本

---
+++ make.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2023-06-16 12:14
+图形界面生成exe文件
+"""
+import os
+import time
+
+os.system(f'pyinstaller -F -w -i cam_capture.ico capture_tk.py -n 摄像头批量截图_{time.strftime("%Y%m%d", time.localtime())}')
