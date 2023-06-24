4d0ec08b16e75d6973d71c6c1cfedc5cede2df2d
sunliguo <sunliguo2006@qq.com>
2023-06-24 20:06:48 +0800

    删除 clock 类

--- capture_tk.py
+++ capture_tk.py
@@ -18,7 +18,6 @@ from tkinter import filedialog, scrolledtext
 from tkinter import ttk, N, S, E, W
 from tkinter.scrolledtext import ScrolledText
 
-
 logger = logging.getLogger()
 logger.setLevel(level=logging.DEBUG)
 
@@ -81,37 +80,6 @@ class LogWidget:
             else:
                 self.display(record)
         self.frame.after(100, self.poll_log_queue)
-
-
-class Clock(threading.Thread):
-    """Class to display the time every seconds
-
-    Every 5 seconds, the time is displayed using the logging.ERROR level
-    to show that different colors are associated to the log levels
-    """
-
-    def __init__(self):
-        super().__init__()
-        self._stop_event = threading.Event()
-
-    def run(self):
-        logger.debug('Clock started')
-        previous = -1
-        while not self._stop_event.is_set():
-            now = datetime.datetime.now()
-        while not self._stop_event.is_set():
-            now = datetime.datetime.now()
-            if previous != now.second:
-                previous = now.second
-                if now.second % 5 == 0:
-                    level = logging.ERROR
-                else:
-                    level = logging.INFO
-                logger.log(level, now)
-            time.sleep(0.2)
-
-    def stop(self):
-        self._stop_event.set()
 
 
 def select_file():
