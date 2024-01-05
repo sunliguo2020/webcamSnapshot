84506685116ac1bdcc2e5db29606872992f4a623
sunliguo <sunliguo2006@qq.com>
2024-01-05 22:02:37 +0800

    配置统一的logger

---
+++ main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2024-01-05 21:07
+"""
+import logging
+
+from mylogger.setlogger import configure_logger
+from Camera import Camera
+# configure_logger()
+
+
+logger = logging.getLogger('camera_logger')
+
+
+def main():
+    print('Hello World!')
+
+
+if __name__ == '__main__':
+    # main()
+    logger.warning('我是谁')
+    cam1 = Camera(camera_type="computer")
+    cam1.is_water_mark = True
+    print(cam1.capture())
