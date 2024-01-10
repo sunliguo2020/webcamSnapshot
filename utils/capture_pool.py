dcfd9c02aeacdbc707bca0e1ea26cf8f8a67f773
sunliguo <sunliguo2006@qq.com>
2024-01-10 18:09:54 +0800

    1、传递参数改变，字典解包 2、logger

--- utils/capture_pool.py
+++ utils/capture_pool.py
@@ -8,9 +8,9 @@ import logging
 from concurrent.futures import ThreadPoolExecutor
 
 from lib.Camera import Camera
-from utils.tool import convert_ip_list
+from utils.tool import get_cam_list
 
-logger = logging.getLogger('camera_log')
+logger = logging.getLogger('camera_logger')
 
 
 def capture_pool(csv_file, *args, **kwargs):
@@ -23,18 +23,16 @@ def capture_pool(csv_file, *args, **kwargs):
     """
     # 1、创建参数列表
     # 返回值为包含ip,password等的字典列表
-    cam_list = convert_ip_list(csv_file)
-    cam_args = [(item['ip'], item['password']) for item in cam_list]
+    cam_list = get_cam_list(csv_file)
     # 2、 创建线程池
     with ThreadPoolExecutor() as pool:
         # results 是返回的结果列表
-        results = pool.map(lambda arg: Camera(*arg, *args, **kwargs).capture(),
-                           cam_args, timeout=20)
+        results = pool.map(lambda inkwargs: Camera(**inkwargs, **kwargs).capture(),
+                           cam_list, timeout=20)
     # 每个ip和结果对应
     # (('172.24.99.101', 'admin'), (-1, None))
-    last_results = list(zip(cam_args, results))
-    # [(('192.168.1.101', 'admi12345'), (-1, None)), (('192.168.1.103', 'admi12345'), (-1, None)), (('192.168.1.105', 'OIBLDQ'), (-1, None)), (('192.168.1.107', 'admi12345'), (-1, None)), (('192.168.1.109', 'admin123'), (-1, None)), (('192.168.1.111', 'FYKWXY'), (-1, None)), (('192.168.1.113', 'admi12345'), (-1, None))]
-    # logger.debug(last_results)
+    last_results = list(zip(cam_list, results))
+    logger.debug(last_results)
 
     for result in last_results:
         logger.debug(f"截图结果:{result}")
