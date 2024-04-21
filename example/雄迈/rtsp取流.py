75be927d3c39e0e3f538cb478d3abf564aa51f19
sunliguo <sunliguo2006@qq.com>
2024-04-21 11:29:48 +0800

    雄迈 rtsp 取流 继续完善

--- example/雄迈/rtsp取流.py
+++ example/雄迈/rtsp取流.py
@@ -4,24 +4,44 @@
 @contact: QQ376440229
 @Created on: 2024-04-21 9:56
 """
+import os
+from datetime import datetime
+
 import cv2
 
 
-def cv2_cap(ip=None):
+def cv2_cap(ip=None, user='admin', password='shiji123'):
     """
 
+    @param password:
+    @param user:
     @param ip:
     @return:
     """
+    datetime_str = datetime.now().strftime("%Y%m%d%H%M%S")
+    date_str = datetime.now().strftime("%Y-%m-%d")
+
+    floder = f"{date_str}"
+    if not os.path.exists(floder):
+        os.makedirs(floder)
+
+    file_name = f"{ip}_{user}_{password}_{datetime_str}"
+
+    file_full_path = os.path.join(floder, file_name)
+
     # 0.sdp主码流 1次码流
-    rtsp_url = f"rtsp://{ip}:554/user=admin&password=&channel=1&stream=0.sdp?"
+    rtsp_url = f"rtsp://{ip}:554/user={user}&password={password}&channel=1&stream=0.sdp?real_stream"
     cap = cv2.VideoCapture(rtsp_url)
+    if not cap.isOpened():
+        return
     ret, frame = cap.read()
-    while ret:
-        ret, frame = cap.read()
-        cv2.imshow("frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    # while ret:
+    #     ret, frame = cap.read()
+    #     cv2.imshow("frame", frame)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    cv2.imencode('.jpg', frame)[1].tofile(file_full_path)
+
     cv2.destroyAllWindows()
     cap.release()
 
