b181c33ea48a66d0fc1db784fd458f8ceada8bac
sunliguo <sunliguo2006@qq.com>
2024-04-21 12:11:17 +0800

    onvif批量截图

---
+++ lib/onvif批量截图.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sunliguo
+@contact: QQ376440229
+@Created on: 2024-04-21 12:01
+"""
+from OnvifClient import  OnvifClient
+from utils.tool import get_cam_list
+if __name__ == '__main__':
+    for item in get_cam_list('../txt/dv.csv'):
+        print(item)
+        onvif_client = OnvifClient(ip=item['ip'],
+                                   port=item['port'],
+                                   username=item['username'],
+                                   password=item['password'])
+        onvif_client.Snapshot()
