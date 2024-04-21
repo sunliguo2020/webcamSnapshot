f69592b31b71a67e5add5376992c27f87fcfd85f
sunliguo <sunliguo2006@qq.com>
2024-04-21 18:19:34 +0800

    onvif 改进

--- lib/onvif批量截图.py
+++ lib/onvif批量截图.py
@@ -4,8 +4,9 @@
 @contact: QQ376440229
 @Created on: 2024-04-21 12:01
 """
-from OnvifClient import  OnvifClient
+from OnvifClient import OnvifClient
 from utils.tool import get_cam_list
+
 if __name__ == '__main__':
     for item in get_cam_list('../txt/dv.csv'):
         print(item)
