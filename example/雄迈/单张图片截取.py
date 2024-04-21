a9864e6df8d2cc4afa6c0833785f045c5250a8f2
sunliguo <sunliguo2006@qq.com>
2024-04-21 08:39:32 +0800

    处理打开失败的异常

--- example/雄迈/单张图片截取.py
+++ example/雄迈/单张图片截取.py
@@ -5,14 +5,11 @@
 @Created on: 2024-04-21 7:55
 """
 import os
-import sys
 from datetime import datetime
 
 import requests
 
 from utils.tool import get_cam_list
-
-print(sys.path)
 
 
 def capture(ip, folder=None):
@@ -33,13 +30,19 @@ def capture(ip, folder=None):
 
     file_full_path = os.path.join(folder, file_name)
     url = f'http://{ip}/webcapture.jpg?command=snap&channel=1'
-    response = requests.get(url, timeout=5)
-    if response.status_code == 200:
-        with open(file_full_path, 'wb') as f:
-            f.write(response.content)
-        print(f'{ip} 抓取成功')
+    try:
+        response = requests.get(url, timeout=5)
+    except ConnectionError as e:
+        print(f"{ip}连接失败：{e}")
+    except Exception as e:
+        print(f'{ip} 打开失败：{e}')
     else:
-        print(f'{ip} 抓取失败')
+        if response.status_code == 200:
+            with open(file_full_path, 'wb') as f:
+                f.write(response.content)
+            print(f'{ip} 抓取成功')
+        else:
+            print(f'{ip} 抓取失败')
 
 
 if __name__ == '__main__':
