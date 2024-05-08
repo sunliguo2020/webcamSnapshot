ef146d4db980ad0d2b14cb642e1bc4118086b3fc
sunliguo <sunliguo2006@qq.com>
2024-05-08 23:12:45 +0800

    1、测试camera-discovery 2、增加windows平台的代码

---
+++ gui/camera-discovery-test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time : 2024/5/8 22:44
+ @Author : sunliguo
+ @Email : sunliguo2006@qq.com
+"""
+# from camera_discovery import CameraDiscovery
+import CameraDiscovery
+CameraDiscovery.ws_discovery()
