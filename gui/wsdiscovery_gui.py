8d09c669addcb570f857fcb49b6ee4085ff16083
sunliguo <sunliguo2006@qq.com>
2024-05-08 22:30:42 +0800

    onvif 搜索小工具v0.1

---
+++ gui/wsdiscovery_gui.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time : 2024/5/8 21:39
+ @Author : sunliguo
+ @Email : sunliguo2006@qq.com
+"""
+import tkinter as tk
+from tkinter import scrolledtext
+
+
+def onvif_find(t1: tk.Text):
+    """
+    查找onvif设备并更新文本框
+    @return:
+    """
+    # http://www.onvif.org/onvif/ver10/network/wsdl/remotediscovery.wsdl
+
+    # from wsdiscovery.threaded import NetworkingThread, MULTICAST_PORT
+    from wsdiscovery import WSDiscovery
+
+    clients = []
+
+    wsd = WSDiscovery()
+    wsd.start()
+    services = wsd.searchServices()
+
+    for service in services:
+        get_ip = str(service.getXAddrs())
+        get_types = str(service.getTypes())
+        clients.append(get_ip)
+
+    wsd.stop()
+    clients.sort()
+    # 清除文本框的当前内容
+    t1.delete('1.0', tk.END)
+
+    # 在文本框中插入搜索到的设备IP
+    for client in clients:
+        t1.insert(tk.END, client + '\n')
+
+
+window = tk.Tk()
+window.geometry('400x400')
+
+window.title('ONVIF设备搜索')
+
+t1 = scrolledtext.ScrolledText(window, height=20, width=50)
+t1.pack(pady=20)
+
+b1 = tk.Button(window, text='搜索', command=lambda: onvif_find(t1))  # 将文本框作为参数传递给函数
+b1.pack(pady=10)
+window.mainloop()
